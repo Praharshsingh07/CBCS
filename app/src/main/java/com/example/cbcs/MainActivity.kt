75985f6547e7a0681a891a22ad4dc71819fdd1ea
Praharshsingh07 <Praharsh.singh@s.amity.edu>
2023-09-23 02:09:22 +0530

    Added Firebase Authenticatio but Splash Screen not working

---
+++ app/src/main/java/com/example/cbcs/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.cbcs
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.cbcs.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth= FirebaseAuth.getInstance()
+        binding.loginbtn.setOnClickListener {
+            val email=binding.editTxtEmailAddress.text.toString()
+            val password=binding.editTxtPassword.text.toString()
+            if (email.isNotEmpty()&&password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this,SelecourseActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this,it.exception.toString(),Toast.LENGTH_LONG).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Feilds can't be empty",Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+}
