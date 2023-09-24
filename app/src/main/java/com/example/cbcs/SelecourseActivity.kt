cc8110491c6869d98cacbb763f4665e1a480ec2e
Kishan <Kishanrathorework@gmail.com>
2023-09-24 06:55:45 +0530

    animation Added

--- app/src/main/java/com/example/cbcs/SelecourseActivity.kt
+++ app/src/main/java/com/example/cbcs/SelecourseActivity.kt
@@ -1,11 +1,24 @@
 package com.example.cbcs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cbcs.databinding.ActivitySelecourseBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SelecourseActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySelecourseBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_selecourse)
+        binding = ActivitySelecourseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.submitBtn.setOnClickListener {
+            val intent = Intent(this,ThankYouActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
