8efad86b0493be34e1ead00170836a2c24edc10d
Praharshsingh <Praharsh.singh@s.amity.edu>
2024-01-24 23:09:12 +0530

    info UI updated

--- app/src/main/java/com/example/cbcs/infoActivity.kt
+++ app/src/main/java/com/example/cbcs/infoActivity.kt
@@ -2,10 +2,15 @@ package com.example.cbcs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
 
 class infoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+        val infoTextView: TextView = findViewById(R.id.info_text)
+        val htmlFormattedString = getString(R.string.info_text_view_content)
+        infoTextView.text = HtmlCompat.fromHtml(htmlFormattedString, HtmlCompat.FROM_HTML_MODE_LEGACY)
     }
 }
