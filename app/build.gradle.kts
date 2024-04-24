5f448a602657b7dabfa6775015438d9501c9af83
Praharsh Raj Singh <praharsh.singh@s.amity.edu>
2024-04-24 21:05:38 +0530

    Newest Update!!! ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.cbcs"
         minSdk = 27
-        targetSdk = 31
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
